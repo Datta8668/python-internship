d1f3d6e00d6075985d6188afe77a292c74a9a1a6
Datta8668 <dattatraybhaganagare3571@gmail.com>
2026-04-27 16:07:59 +0530

    python

---
+++ chapter11/problem3.py
@@ -0,0 +1,6 @@
+class Employee:
+    salary = 234
+    increment = 20
+
+e = Employee
+
---
+++ chapter11/problem4.py
@@ -0,0 +1,16 @@
+class Employee:
+    salary = 234
+    increment = 20
+
+    @property
+    def SalaryAfterIncrement(self):
+        return self.salary + self.salary * (self.increment / 100)
+
+    @SalaryAfterIncrement.setter
+    def salaryAferIncrement(self,salary):
+        self.increment = ((salary/self.salary)-1)*100
+
+e = Employee()
+# print(e.SalaryAfterIncrement)
+e.salaryAferIncrement = 280.8
+print(e.increment)
